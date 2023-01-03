eaa7ac3118b46c379d4174cc3b8f446bcb023e17
knvsousa <kevin.sousa@lognature.com.br>
2023-01-03 12:47:21 -0300

    News services

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import connectDatabase from './src/database/db.js'
 import dotenv from 'dotenv'
 
 import useRoute from './src/routes/user.route.js'
-import authRouter from './src/routes/auth.route.js'
+import authRoute from './src/routes/auth.route.js'
+import newsRoute from './src/routes/news.router.js'
 
 dotenv.config()
 
@@ -13,7 +14,8 @@ const port = process.env.PORT || 3000
 connectDatabase()
 app.use(express.json())
 app.use('/user', useRoute)
-app.use('/auth', authRouter)
+app.use('/auth', authRoute)
+app.use('/news', newsRoute)
 
 
 app.listen(port, () => console.log(`ta na porta ${port}`))
--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService } from '../services/news.service'
+import { createService, findAllService } from '../services/news.service.js'
 
 const create = async (req, res) => {
   try {
@@ -12,19 +12,22 @@ const create = async (req, res) => {
       title,
       text,
       banner,
-      id: 'id fake',
+      user: { _id: '63b3371a01ec7a21fdf066af' },
     })
 
-    res.send(201)
+    res.sendStatus(201)
   } catch (error) {
     res.status(500).send({ message: error.message })
   }
 }
 
 const findAll = async (req, res) => {
-  const news = []
 
-  res.send(201)
+  const news = await findAllService()
+  if (news.length === 0) {
+    res.statusSend(400)
+  }
+  res.send(news)
 }
 
 export { create, findAll }
